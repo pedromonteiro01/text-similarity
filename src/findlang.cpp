337627419513fa73f95cc154ff93ac518f823176
Pedro Monteiro <pmapm@ua.pt>
2023-05-29 20:02:33 +0100

    add more comments to findlang

--- src/findlang.cpp
+++ src/findlang.cpp
@@ -45,6 +45,7 @@ char predict_char(const vector<int> &positions, vector<string> &chunks)
 // function to estimate bits for a given language model and target data
 double estimate_bits(map<string, vector<int>> &chunk_positions, vector<string> &chunks, string &target_data, int k, double alpha, int fail_threshold, int N)
 {
+    // declare variables
     int hits = 0;
     int fails = 0;
     double estimated_bits = 0;
@@ -52,20 +53,21 @@ double estimate_bits(map<string, vector<int>> &chunk_positions, vector<string> &
 
     for (int i = 0; i < target_data.size() - k; ++i)
     {
-        string current_chunk = target_data.substr(i, k);
+        string current_chunk = target_data.substr(i, k); // get current chunk from the target data
 
         if (chunk_positions.find(current_chunk) == chunk_positions.end())
         {
-            estimated_bits += log2(N);
+            estimated_bits += log2(N); // add log2(N) when new sequencies are found
             continue;
         }
 
-        if (i + k >= target_data.size())
+        if (i + k >= target_data.size()) // reach end of file
             break;
 
-        char solution = target_data[(i + k)];
-        char prediction = predict_char(chunk_positions[current_chunk], chunks);
+        char solution = target_data[(i + k)]; // get the actual symbol
+        char prediction = predict_char(chunk_positions[current_chunk], chunks); // get predicted symbol from vector
 
+        // check for hits and fails
         if (prediction == solution)
         {
             hits++;
@@ -78,13 +80,13 @@ double estimate_bits(map<string, vector<int>> &chunk_positions, vector<string> &
             auto it = positions_count.find(key);
             if (it != positions_count.end())
             {
-                if (it->second < fail_threshold)
+                if (it->second < fail_threshold) // check if position fails count is below the threshold
                 {
                     it->second++;
                 }
                 else
                 {
-                    if (!chunk_positions[current_chunk].empty())
+                    if (!chunk_positions[current_chunk].empty()) // delete position if above fails threshold
                         chunk_positions[current_chunk].erase(chunk_positions[current_chunk].begin());
                 }
             }
@@ -95,7 +97,7 @@ double estimate_bits(map<string, vector<int>> &chunk_positions, vector<string> &
         }
 
         double prob = (hits + alpha) / (hits + fails + 2 * alpha);
-        estimated_bits += -log2(prob);
+        estimated_bits += -log2(prob); // calculate estimated bits
     }
 
     return estimated_bits;
@@ -119,6 +121,9 @@ int main(int argc, char *argv[])
     // read target file
     string target_data = read_file(target_filename);
 
+    double min_estimated_bits = numeric_limits<double>::max();
+    string best_language;
+
     // read reference directory
     DIR *dir;
     struct dirent *ent;
@@ -130,14 +135,12 @@ int main(int argc, char *argv[])
             string reference_filename = string(ent->d_name);
             if (reference_filename == "." || reference_filename == "..") continue; // skip . and ..
 
-            // read reference file
-            string reference_data = read_file(reference_directory + "/" + reference_filename);
+            string reference_data = read_file(reference_directory + "/" + reference_filename); // read reference file
 
             // calculate the number of distinct characters in the reference file
             int N = count_distinct_chars(reference_data);
 
-            // create copy model using the reference data
-            map<string, vector<int>> chunk_positions;
+            map<string, vector<int>> chunk_positions; // create copy model using the reference data
             vector<string> chunks;
 
             for (int i = 0; i < reference_data.size() - k; ++i)
@@ -147,20 +150,27 @@ int main(int argc, char *argv[])
 
                 if (chunk_positions.find(current_chunk) == chunk_positions.end())
                 {
-                    chunk_positions[current_chunk] = vector<int>{i};
+                    chunk_positions[current_chunk] = vector<int>{i}; // add chunk to the map
                     continue;
                 }
 
-                chunk_positions[current_chunk].push_back(i);
+                chunk_positions[current_chunk].push_back(i); // if chunk exists on map, add position
             }
 
             // estimate bits for this language
             double estimated_bits = estimate_bits(chunk_positions, chunks, target_data, k, alpha, fail_threshold, N);
 
+            if (estimated_bits < min_estimated_bits)
+            {
+                min_estimated_bits = estimated_bits;
+                best_language = reference_filename;
+            }
+
             // print estimated bits for this language
             cout << "Estimated total bits for " << reference_filename << ": " << estimated_bits << endl;
         }
         closedir(dir);
+        cout << "The text is likely to be written in " << best_language << ": " << min_estimated_bits << endl;
     }
     else
     {
